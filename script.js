0d125cc7f9338509a9e2167509a8362b7d5b9103
AKShaheen <101220437+AKShaheen@users.noreply.github.com>
2024-03-31 06:48:54 +0200

    Adding The Main Logic
    Creating a Function that runs a game of five rounds and has a tie breaker

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
-const choices = ['rock','paper','scissors',];
+const choices = ['rock','paper','scissors'];
+function getPlayerChoice(){
+    const playerChoice = prompt('Please choose rock, paper, or scissors');
+    return playerChoice.toLowerCase();
+}
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)];
 }
@@ -8,19 +12,50 @@ function playRound(playerSelection, computerSelection){
         return 'tie';
     }
     else if(playerSelection === 'rock' && computerSelection ==='scissors'){
-        return 'lose';
+        return 'win';
     }
     else if(playerSelection ==='scissors' && computerSelection === 'paper'){
-        return 'lose';
+        return 'win';
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        return 'lose';
+        return 'win';
     }
     else{
-        return 'win';
+        return 'lose';
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame(flag){
+    let win=0,lose=0,tie=0;
+    while(flag !=0){
+        const computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        const playerChoice = getPlayerChoice();
+        const result = playRound(playerChoice, computerChoice);
+        if(result === 'win'){
+            console.log('You Win!');
+            win ++;
+            flag--;
+        }
+        else if(result === 'lose'){
+            console.log('You Lose!');
+            lose++;
+            flag--;
+        }
+        else if(result === 'tie'){
+            console.log('You Tied!');
+            tie++;
+            flag--;
+        }
+    }
+    console.log(win,lose,tie);
+    if(win > lose && win > tie){
+        alert('You Win!');
+    }else if(lose > tie && lose > win){
+        alert('You Lose!');
+    }else{
+        alert('Tie Breaker!');
+        playGame(1);
+    }   
+}
+playGame(5);
